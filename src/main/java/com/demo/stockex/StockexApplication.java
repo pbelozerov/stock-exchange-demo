81ce36eba578af5fa09885e739f879d647fb9dc6
Pavel Belozerov <pabelozerov@gmail.com>
2020-10-13 14:23:26 +0300

    Initial commit

---
+++ src/main/java/com/demo/stockex/StockexApplication.java
@@ -0,0 +1,13 @@
+package com.demo.stockex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StockexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/demo/stockex/StockexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.stockex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
